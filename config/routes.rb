9ad2783b283da248c9a1777660daf2aaeb7f4966
yliou <yliou@berkeley.edu>
2017-09-27 16:29:17 -0700

    added last question functionality, done with assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   put 'age', to: 'pages#person'
+  get 'me', to: 'pages#me'
   post 'stringify', to: 'pages#stringify'
 end
