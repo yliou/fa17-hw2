d648e0f9ea62a40f1f979bb12d29944576136d33
yliou <yliou@berkeley.edu>
2017-09-27 15:04:24 -0700

    finished question 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'age', to: 'pages#age'
+  put 'age', to: 'pages#person'
   post 'stringify', to: 'pages#stringify'
 end
